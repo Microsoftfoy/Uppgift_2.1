214e1dcce925ea292ad4ecf643a1cec03955e9ff
ercr07001 <ercr07001@E-PW05CGR0>
2024-10-09 12:04:52 +0200

    Add project files.

---
+++ Uppgift_2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vänligen skriv in ditt favorit bok");
+            string favoritbok = Console.ReadLine();
+
+            Console.WriteLine("din favorit bok är" + " " + favoritbok);
+
+            Console.ReadKey();
+        }
+    }
+}
+
